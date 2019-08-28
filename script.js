64059ae989b6b41efbf9813db5cf5f3230ab5d47
jaredWu0805 <jared810805@gmail.com>
2019-08-29 02:04:02 +0800

    added inital files

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".dropdown-trigger").dropdown({ hover: false,coverTrigger: false });
+})
+
+   
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
